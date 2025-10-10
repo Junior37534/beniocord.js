26e76dcf06613178b342fefb908fa06de2d0ac08
Junior <junior.silva2006@outlook.com>
2025-10-10 19:26:04 -0400

    small fix

--- structures/User.js
+++ structures/User.js
@@ -3,13 +3,14 @@ class User {
     this.id = data.id;
     this.username = data.username;
     this.displayName = data.display_name;
-    this.email = data.email;
     this.avatarUrl = data.avatar_url;
     this.status = data.status || "offline";
     this.emblems = data.emblems || [];
     this.lastSeen = data.last_seen;
     this.createdAt = data.created_at;
-    this.updatedAt = data.updated_at;
+    this.isBot = data.is_bot || false;
+    // this.email = data.email;
+    // this.updatedAt = data.updated_at;
   }
 }
 
